087c7690b98046cf4a1ede18b5317692d3f6ca1a
EnriqueIzel2 <eleaoizel@gmail.com>
2022-09-10 22:03:10 -0400

    feat: create function to add text at result text

--- app/src/main/java/com/enrique/calculadoradehoras/MainActivity.kt
+++ app/src/main/java/com/enrique/calculadoradehoras/MainActivity.kt
@@ -2,10 +2,18 @@ package com.enrique.calculadoradehoras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enrique.calculadoradehoras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.btnCalculaHoras.setOnClickListener {
+      binding.txtResultado.setText("Novo resultado")
+    }
   }
 }
