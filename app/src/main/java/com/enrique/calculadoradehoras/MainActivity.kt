33c281ae106c3cf8cf068fc10b9ca375b9787ce7
EnriqueIzel2 <eleaoizel@gmail.com>
2022-11-15 16:55:49 -0400

    feat: create function for time convertion and clear fields

--- app/src/main/java/com/enrique/calculadoradehoras/MainActivity.kt
+++ app/src/main/java/com/enrique/calculadoradehoras/MainActivity.kt
@@ -2,6 +2,7 @@ package com.enrique.calculadoradehoras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.enrique.calculadoradehoras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,58 @@ class MainActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     binding.btnCalculaHoras.setOnClickListener {
-      binding.txtResultado.setText("Novo resultado")
+      val horasEntrada = binding.editHorasEntrada.text.toString()
+      val minutosEntrada = binding.editMinutosEntrada.text.toString()
+      val horasTrabalhadas = binding.editHorasTrabalhadas.text.toString()
+      val minutosTrabalhados = binding.editMinutosTrabalhados.text.toString()
+
+      if (
+        horasEntrada.isNotEmpty() &&
+        minutosEntrada.isNotEmpty() &&
+        horasTrabalhadas.isNotEmpty() &&
+        minutosTrabalhados.isNotEmpty()
+      ) {
+        val horarioSaida =
+          timeConversor(
+            horasEntrada.toInt(),
+            minutosEntrada.toInt(),
+            horasTrabalhadas.toInt(),
+            minutosTrabalhados.toInt()
+          )
+        val message = "Horário de saída ${horarioSaida[0]}:${horarioSaida[1]}"
+        limpaCampos()
+        binding.txtResultado.setText(message)
+      } else {
+        Toast.makeText(this, "Todos os campos devem ser preenchidos", Toast.LENGTH_SHORT)
+          .show()
+      }
     }
   }
+
+  private fun timeConversor(
+    horas: Int,
+    minutos: Int,
+    hTrabalhadas: Int,
+    mTrabalhadas: Int
+  ): List<Int> {
+    val totalMinutosParaTrabalhar = hTrabalhadas * 60 + mTrabalhadas
+    val totalMinutos = horas * 60 + minutos + totalMinutosParaTrabalhar
+    val horaSaida = totalMinutos / 60
+    val minutoSaida = totalMinutos % 60
+
+    return listOf(horaSaida, minutoSaida)
+  }
+
+  private fun limpaCampos() {
+    with(binding) {
+      editHorasEntrada.setText("")
+      editMinutosEntrada.setText("")
+      editHorasTrabalhadas.setText("")
+      editMinutosTrabalhados.setText("")
+    }
+  }
+
+  /*
+  TODO: validar máximo de 24 horas
+  */
 }
