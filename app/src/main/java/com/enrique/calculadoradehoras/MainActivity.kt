88f732e507f68a83e6981086228272627e90201b
EnriqueIzel2 <eleaoizel@gmail.com>
2022-11-15 17:21:22 -0400

    feat: create function to check if the time's type is valid

--- app/src/main/java/com/enrique/calculadoradehoras/MainActivity.kt
+++ app/src/main/java/com/enrique/calculadoradehoras/MainActivity.kt
@@ -25,16 +25,26 @@ class MainActivity : AppCompatActivity() {
         horasTrabalhadas.isNotEmpty() &&
         minutosTrabalhados.isNotEmpty()
       ) {
-        val horarioSaida =
-          timeConversor(
+        if (isValidTime(
             horasEntrada.toInt(),
             minutosEntrada.toInt(),
             horasTrabalhadas.toInt(),
             minutosTrabalhados.toInt()
           )
-        val message = "Horário de saída ${horarioSaida[0]}:${horarioSaida[1]}"
-        limpaCampos()
-        binding.txtResultado.setText(message)
+        ) {
+          val horarioSaida =
+            timeConversor(
+              horasEntrada.toInt(),
+              minutosEntrada.toInt(),
+              horasTrabalhadas.toInt(),
+              minutosTrabalhados.toInt()
+            )
+          val message = "Horário de saída ${horarioSaida[0]}:${horarioSaida[1]}"
+          limpaCampos()
+          binding.txtResultado.setText(message)
+        } else {
+          Toast.makeText(this, "Tipo de horário errado, tente novamente", Toast.LENGTH_SHORT).show()
+        }
       } else {
         Toast.makeText(this, "Todos os campos devem ser preenchidos", Toast.LENGTH_SHORT)
           .show()
@@ -56,6 +66,23 @@ class MainActivity : AppCompatActivity() {
     return listOf(horaSaida, minutoSaida)
   }
 
+  private fun isValidTime(
+    hEntrada: Int,
+    mEntrada: Int,
+    hTrabalhadas: Int,
+    mTrabalhados: Int
+  ): Boolean {
+    if (
+      hEntrada >= 0 && hEntrada <= 24 &&
+      mEntrada >= 0 && mEntrada <= 60 &&
+      hTrabalhadas >= 0 && hTrabalhadas <= 24 &&
+      mTrabalhados >= 0 && mTrabalhados <= 60
+    ) {
+      return true
+    }
+    return false
+  }
+
   private fun limpaCampos() {
     with(binding) {
       editHorasEntrada.setText("")
@@ -64,8 +91,4 @@ class MainActivity : AppCompatActivity() {
       editMinutosTrabalhados.setText("")
     }
   }
-
-  /*
-  TODO: validar máximo de 24 horas
-  */
 }
